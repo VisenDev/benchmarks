1be85467dfa60abdee3e3de9e5d846d97ccf500e
Robert Burnett <dr.slate2@gmail.com>
2025-09-02 14:01:05 -0500

    clean up the implementation to produce simpler code

--- brainfuck/bf.lisp
+++ brainfuck/bf.lisp
@@ -1,4 +1,4 @@
-(declaim (optimize (speed 3) (safety 0)))
+(declaim (optimize (speed 3) (safety 1)))
 
 (declaim (ftype (function (stream character)) count-duplicates))
 (defun count-duplicates (stream char)
@@ -20,46 +20,55 @@
     :while (not (member ch (list #\> #\< #\+ #\- #\[ #\. #\,)))
     :do (read-char stream))
   (loop
+    :with codes = '()
     :while (listen stream)
     :for ch = (read-char stream)
     :while (not (eq #\] ch))
     :while (not (eq #\Newline ch))
-    :collect
-    (ecase ch
-      (#\> `(incf ptr ,(count-duplicates stream #\>)))
-      (#\< `(decf ptr ,(count-duplicates stream #\<)))
-      (#\+ `(incf (aref arr ptr) ,(count-duplicates stream #\+)))
-      (#\- `(decf (aref arr ptr) ,(count-duplicates stream #\-)))
-      (#\[ (let* ((start (gensym))
-                  (end (gensym))
-                  )
-             `(tagbody
-                 ,start 
-                 (when (= 0 (aref arr ptr))
-                   (go ,end))
-                 ,@(opcodes stream)
-                 (go ,start)
-                 ,end)))
-                 
-      (#\. `(format t "~c" (code-char (aref arr ptr))))
-      (#\, `(setf (aref arr ptr) (read-byte *standard-input*)))
-      )
+    :do
+       (ecase ch
+         (#\> (push `(incf ptr ,(count-duplicates stream #\>)) codes))
+         (#\< (push `(decf ptr ,(count-duplicates stream #\<)) codes))
+         (#\+ (push `(incf (aref arr ptr) ,(count-duplicates stream #\+)) codes))
+         (#\- (push `(decf (aref arr ptr) ,(count-duplicates stream #\-)) codes))
+         (#\[ (let* ((start (gensym))
+                     (end (gensym))
+                     )
+                (push start codes)
+                (push `(when (= 0 (aref arr ptr))
+                         (go ,end))
+                      codes)
+                (dolist (val (opcodes stream))
+                  (push val codes))
+                (push `(go ,start) codes)
+                (push end codes)))
+         
+         (#\. (push `(format stdout "~c" (code-char (aref arr ptr))) codes))
+         (#\, (push `(setf (aref arr ptr) (print (- (char-code (read-char stdin))
+                                             (char-code #\0))))
+                                              codes))
+         )
+    :finally (return (nreverse codes))
     ))
 
 
 
 
 (defun codegen (stream)
-   (eval `(defun program ()
+  (eval
+   `(lambda (&optional (stdin t) (stdout t))
+      (declare (ignorable stdin stdout))
       (let ((arr (make-array 30000 :element-type `(integer 0 255)
                                    :initial-element 0))
             (ptr 0)
-                  )
+            )
         (declare (type fixnum ptr))
-        ,@(opcodes stream)
-      
-              ))))
+        (tagbody 
+           ,@(opcodes stream)
+           )
+        ))))
 
+(defparameter *hello* "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.")
 
 (defun main ()
   (let* ((filename (first uiop:*command-line-arguments*))
